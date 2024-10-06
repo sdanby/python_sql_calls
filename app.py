35fee1bd31a4bf96a7cd051ef9fd9f3b989c2467
sdanby <61995820+sdanby@users.noreply.github.com>
2024-10-06 18:27:41 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from flask_cors import CORS  # Import CORS
 
 app = Flask(__name__)
+CORS(app)  # Enable CORS for all routes
 
 @app.route('/')
 def hello():
-    return 'Hello, World!'
+    return 'Hello, World!!!!'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
