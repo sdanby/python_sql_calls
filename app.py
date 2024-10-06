6589f507c5cbe149cccce02dd8334df30ed850e1
sdanby <61995820+sdanby@users.noreply.github.com>
2024-10-06 18:43:51 +0100

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ CORS(app)  # Enable CORS for all routes
 
 @app.route('/')
 def hello():
-    return 'Hello, World!!!!'
+    return 'How quickly will this update the front-end?'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
