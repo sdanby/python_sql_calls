bc4296c8bddeee94d47570fd351289a1118be9ff
sdanby <61995820+sdanby@users.noreply.github.com>
2024-10-08 23:36:28 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,11 @@ from flask_cors import CORS  # Import CORS
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
 
+# Replace the following credentials with your actual database credentials
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://parkrundata_user:m3UE0JWilwRNS1MBVgN2kr0BnIOVZUmH@host:port/dbname'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
 @app.route('/')
 def hello():
     return 'How quickly will this update the front-end?'
