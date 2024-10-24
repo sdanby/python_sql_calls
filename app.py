d810d40176bb15d2aace328eaa8146935b20f7be
sdanby <61995820+sdanby@users.noreply.github.com>
2024-10-24 01:03:46 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS  # Make sure to import CORS
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Date
 
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
